1d73d5ffac03458365c34cf4fc57ac510a97d4f1
mareadelmar <mar.ortega.v@gmail.com>
2022-01-10 17:49:19 -0300

    Create redux action test

---
+++ src/__test__/actions/actions.test.js
@@ -0,0 +1,15 @@
+import actions from '../../actions';
+import ProductMock from '../../__mocks__/ProductMock';
+
+describe('Actions', () => {
+  test('addToCart Action', () => {
+    // utilizamos la estructura de product para testear el payload
+    const payload = ProductMock;
+    const expected = {
+      type: 'ADD_TO_CART',
+      payload,
+    };
+
+    expect(actions.addToCart(payload)).toEqual(expected);
+  });
+});

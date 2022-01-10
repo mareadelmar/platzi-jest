878c9128d13a0578e4c34bbf3f4bfbcbe4e097f4
mareadelmar <mar.ortega.v@gmail.com>
2022-01-10 18:34:06 -0300

    Add configuration to intercept requests

--- src/__test__/setupTest.js
+++ src/__test__/setupTest.js
@@ -3,3 +3,6 @@ import Adapter from 'enzyme-adapter-react-16';
 
 // configuración: le pasamos el adapter
 configure({ adapter: new Adapter() });
+
+// capturar las peticiones: en vez de fetch, que utilice este paquete
+global.fetch = require('jest-fetch-mock');

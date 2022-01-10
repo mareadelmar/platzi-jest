5abe94fb46d385aaaf6cf648839c1742ff59d682
mareadelmar <mar.ortega.v@gmail.com>
2022-01-10 13:50:51 -0300

    Create Header test using the provider mock

---
+++ src/__test__/Header.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ProviderMock from '../__mocks__/ProviderMock';
+import Header from '../components/Header';
+
+describe('<Header />', () => {
+  test('Render del componente Header', () => {
+    const header = shallow(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+
+    // chequear que se está montando
+    expect(header.length).toEqual(1);
+  });
+
+  test('Render del título', () => {
+    const header = mount(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+
+    expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
